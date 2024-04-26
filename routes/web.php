a024ac71711d9b08210b774eb25ce4c7b290c9de
melpenyogi <melpenyogi2019@gmail.com>
2024-04-27 07:56:24 +0900

    feat: membuat footer menggunakan tailwind.css

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'welcome';
+});
+
+Route::get('/maikel', function () {
+    return view('maikel');
+});
+
+Route::get('/rebly', function () {
+    return view('rebly');
+});
+
+Route::get('/melpen', function () {
+    return view('melpen');
+});
+
+Route::get('/marthen', function () {
+    return view('marthen');
+});
+
+Route::get('/acho', function () {
+    return view('acho');
+});
+
+Route::get('/janzen', function () {
+    return view('janzen.index');
+});
